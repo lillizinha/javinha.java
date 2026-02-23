3a8eb6fa35681ea85d5616de2d76f18153e6cddb
lillizinha <olafhmmk@gmail.com>
2026-02-23 16:17:15 -0300

    Criar divisao.java
    atvdowendelll!!!

---
+++ divisao.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class divisao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+
+        int resto = num1 % num2;
+
+        System.out.println("O resto da divisão é: " + resto);
+
+        scanner.close();
+    }
+}
