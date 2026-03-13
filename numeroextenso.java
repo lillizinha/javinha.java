4d42c95a527d131cd0459cba90a9883bbaddf875
lillizinha <olafhmmk@gmail.com>
2026-03-13 09:32:20 -0300

    atualizando o numeroextenso.java

--- numeroextenso.java
+++ numeroextenso.java
@@ -1,57 +1,86 @@
 import java.util.Scanner;
 
 public class numeroextenso {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int num;
 
-        System.out.print("Digite um número de 1 a 10: ");
-        num = sc.nextInt();
+        int numero;
+        int dezena;
+        int unidade;
 
-        switch (num) {
-            case 1:
-                System.out.println("Um");
-                break;
+        System.out.print("Digite um número de 1 a 100: ");
+        numero = sc.nextInt();
 
-            case 2:
-                System.out.println("Dois");
-                break;
+        if(numero == 100){
+            System.out.println("Cem");
+        }
+        else if(numero >= 1 && numero <= 99){
 
-            case 3:
-                System.out.println("Três");
-                break;
+            dezena = numero / 10;
+            unidade = numero % 10;
 
-            case 4:
-                System.out.println("Quatro");
-                break;
+            switch(dezena){
 
-            case 5:
-                System.out.println("Cinco");
-                break;
+                case 0:
+                    switch(unidade){
+                        case 1: System.out.println("Um"); break;
+                        case 2: System.out.println("Dois"); break;
+                        case 3: System.out.println("Três"); break;
+                        case 4: System.out.println("Quatro"); break;
+                        case 5: System.out.println("Cinco"); break;
+                        case 6: System.out.println("Seis"); break;
+                        case 7: System.out.println("Sete"); break;
+                        case 8: System.out.println("Oito"); break;
+                        case 9: System.out.println("Nove"); break;
+                    }
+                    break;
 
-            case 6:
-                System.out.println("Seis");
-                break;
+                case 1:
+                    switch(unidade){
+                        case 0: System.out.println("Dez"); break;
+                        case 1: System.out.println("Onze"); break;
+                        case 2: System.out.println("Doze"); break;
+                        case 3: System.out.println("Treze"); break;
+                        case 4: System.out.println("Quatorze"); break;
+                        case 5: System.out.println("Quinze"); break;
+                        case 6: System.out.println("Dezesseis"); break;
+                        case 7: System.out.println("Dezessete"); break;
+                        case 8: System.out.println("Dezoito"); break;
+                        case 9: System.out.println("Dezenove"); break;
+                    }
+                    break;
 
-            case 7:
-                System.out.println("Sete");
-                break;
+                case 2: System.out.print("Vinte"); break;
+                case 3: System.out.print("Trinta"); break;
+                case 4: System.out.print("Quarenta"); break;
+                case 5: System.out.print("Cinquenta"); break;
+                case 6: System.out.print("Sessenta"); break;
+                case 7: System.out.print("Setenta"); break;
+                case 8: System.out.print("Oitenta"); break;
+                case 9: System.out.print("Noventa"); break;
+            }
 
-            case 8:
-                System.out.println("Oito");
-                break;
+            if(dezena >= 2 && unidade != 0){
 
-            case 9:
-                System.out.println("Nove");
-                break;
+                System.out.print(" e ");
 
-            case 10:
-                System.out.println("Dez");
-                break;
+                switch(unidade){
+                    case 1: System.out.print("Um"); break;
+                    case 2: System.out.print("Dois"); break;
+                    case 3: System.out.print("Três"); break;
+                    case 4: System.out.print("Quatro"); break;
+                    case 5: System.out.print("Cinco"); break;
+                    case 6: System.out.print("Seis"); break;
+                    case 7: System.out.print("Sete"); break;
+                    case 8: System.out.print("Oito"); break;
+                    case 9: System.out.print("Nove"); break;
+                }
+            }
 
-            default:
-                System.out.println("Número fora do intervalo.");
+        } else {
+            System.out.println("Número inválido.");
         }
 
         sc.close();
