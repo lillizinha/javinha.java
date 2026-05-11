e7bd098b9375def543326c6862c26b9977a0f9cf
lillizinha <olafhmmk@gmail.com>
2026-05-11 16:31:37 -0300

    atualizano
    atvherançaa

--- Aluno.java
+++ Aluno.java
@@ -1,35 +1,13 @@
-import java.time.LocalTime;
+class Aluno extends Pessoa {
+    String matricula;
 
-public class Aluno{
-    private String nome;
-    private int mtr;
-    private String dtNascimento;
-    private String telefone;
-    private String nomeResponsa;
-
-    public Aluno(String nome, int mtr, String dtNascimento, String telefone, String nomeResponsa){
-        this.nome = nome;
-        this.mtr = mtr;
-        this.dtNascimento = dtNascimento;
-        this.telefone = telefone;
-        this.nomeResponsa = nomeResponsa;
+    Aluno(String nome, String fone, String cpf, String matricula) {
+        super(nome, fone, cpf);
+        this.matricula = matricula;
     }
 
-    public String apresenteSe(){
-        return "o aluno eh:" + this.nome + "   | seu telefone eh: " + this.telefone + 
-        "  | sua data de nascimento: " + this.dtNascimento + "  | sua matricula";
-    }
-
-    public String Saudacao(){
-        LocalTime agora = LocalTime.now();
-        int hora = agora.getHour();
-        if(hora >= 6 && hora < 12){
-            return "Bomdia";
-        } else if(hora >= 12 && hora < 18){
-            return "Boatarde";
-        } else {
-            return "Boanoite";
-        }
-        
+    void aprende() {
+        System.out.println(nome + " está aprendendo.");
     }
 }
+
