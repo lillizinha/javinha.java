75f5f2253ce142ec96444edde61054aaae4ba22a
lillizinha <olafhmmk@gmail.com>
2026-05-18 15:26:19 -0300

    Atualizar o Pessoa.java
    atualizando

--- Pessoa.java
+++ Pessoa.java
@@ -1,15 +1,33 @@
-    class Pessoa {
-    String nome;
-    String fone;
-    String cpf;
+import java.util.Scanner;
 
-    Pessoa(String nome, String fone, String cpf) {
+public class Pessoa {
+
+    private String nome;
+    private String fone;
+    private String cpf;
+
+    public Pessoa(String nome, String fone, String cpf) {
+
         this.nome = nome;
         this.fone = fone;
         this.cpf = cpf;
     }
 
-    void saudacao() {
-        System.out.println("Olá, meu nome é " + nome);
+    public String getNome() {
+        return nome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    
+    public String saudacao() {
+
+        return "olá, meu nome é " + nome;
     }
 }
