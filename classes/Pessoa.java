ba0ef4eb3fc73c9413193723621b731e2c5150f5
lillizinha <olafhmmk@gmail.com>
2026-05-20 08:26:17 -0300

    Change Pessoa class to abstract and add methods

--- classes/Pessoa.java
+++ classes/Pessoa.java
@@ -1,33 +1,65 @@
 import java.util.Scanner;
 
-public class Pessoa {
-
+public abstract class Pessoa {
     private String nome;
     private String fone;
     private String cpf;
-
+    
+    // Construtor
     public Pessoa(String nome, String fone, String cpf) {
-
         this.nome = nome;
         this.fone = fone;
         this.cpf = cpf;
     }
-
+    
     public String getNome() {
         return nome;
     }
-
+    
     public String getFone() {
         return fone;
     }
-
+    
     public String getCpf() {
         return cpf;
     }
-
     
-    public String saudacao() {
-
-        return "olá, meu nome é " + nome;
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+    
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    public abstract String saudacao();
+    
+  
+    public String obterInformacoes() {
+        return String.format("Nome: %s | Telefone: %s | CPF: %s", nome, fone, cpf);
+    }
+    
+    
+    public void apresentarSe() {
+        System.out.println(saudacao());
+        System.out.println(obterInformacoes());
+    }
+    
+  
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Pessoa pessoa = (Pessoa) obj;
+        return cpf.equals(pessoa.cpf);
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%s [%s]", getClass().getSimpleName(), nome);
     }
 }
