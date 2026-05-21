6723987053c2f96f219911b0145baeadab306426
lillizinha <olafhmmk@gmail.com>
2026-05-21 19:10:03 -0300

    Atualizar o Pessoa.java
    atualizandoo

--- classes/Pessoa.java
+++ classes/Pessoa.java
@@ -1,65 +1,41 @@
 import java.util.Scanner;
 
-public abstract class Pessoa {
+public class Pessoa {
+
     private String nome;
     private String fone;
     private String cpf;
-    
-    // Construtor
+
     public Pessoa(String nome, String fone, String cpf) {
+
         this.nome = nome;
         this.fone = fone;
         this.cpf = cpf;
     }
-    
+
     public String getNome() {
         return nome;
     }
-    
+
     public String getFone() {
         return fone;
     }
-    
+
     public String getCpf() {
         return cpf;
     }
+
     
-    public void setNome(String nome) {
-        this.nome = nome;
+
+    public String saudacao() {
+        return "Olá, meu nome é " + nome;
     }
-    
-    public void setFone(String fone) {
-        this.fone = fone;
+
+    public String saudacao(Aluno aluno) {
+        return "Olá, meu nome é " + nome;
     }
-    
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    
-    public abstract String saudacao();
-    
-  
-    public String obterInformacoes() {
-        return String.format("Nome: %s | Telefone: %s | CPF: %s", nome, fone, cpf);
-    }
-    
-    
-    public void apresentarSe() {
-        System.out.println(saudacao());
-        System.out.println(obterInformacoes());
-    }
-    
-  
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Pessoa pessoa = (Pessoa) obj;
-        return cpf.equals(pessoa.cpf);
-    }
-    
-    @Override
-    public String toString() {
-        return String.format("%s [%s]", getClass().getSimpleName(), nome);
+
+    public String saudacao(Professor professor) {
+        return "Olá, meu nome é " + nome;
     }
 }
